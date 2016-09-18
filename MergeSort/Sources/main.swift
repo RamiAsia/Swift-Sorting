393b516ea69bd12710f546a6955a4662be5e72b0
Rami Asia <ramirayyan.rami.t.asia@gmail.com>
2016-09-18 05:05:10 +0300

    Added comments in the Merge Sort algorithm implementation for added clarity

--- MergeSort/Sources/main.swift
+++ MergeSort/Sources/main.swift
@@ -3,32 +3,46 @@ class MergeSort {
     func mergeSort<T: Comparable> (_ array: [T]) -> [T] {
         var sortedArray = array
         if array.count > 1 {
+            // Recursively sort the left half of the array, followed by the right half
             let leftArray = mergeSort(Array(array[0..<(array.count/2)]))
             let rightArray = mergeSort(Array(array[(array.count/2)..<array.count]))
+
+            // Once each side is sorted, call the mergeArrays method to merge them into one sorted array
             sortedArray = mergeArrays(arrayOne: leftArray, arrayTwo: rightArray)
         }
         return sortedArray
     }
 
     func mergeArrays<T: Comparable>(arrayOne: [T], arrayTwo: [T]) -> [T] {
+        // resultArray is where we'll be inserting our values from the two arrays to merge
         var resultArray = [T]()
 
         var indexOne = 0, indexTwo = 0
 
+        /* Traverse the arrays, keeping track of their indices to make sure both arrays
+        ** are completely emptied into the merged array in the correct order
+        */
         while indexOne < arrayOne.count {
-
+            /* Insert the second array's values into the merged array until it has a value greater than
+            ** the one currently being checked in the first array
+            */
             while indexTwo < arrayTwo.count && arrayTwo[indexTwo] <= arrayOne[indexOne] {
                 resultArray += [arrayTwo[indexTwo]]
                 indexTwo += 1
             }
+            /* Once all values less than the first array's current value have been
+            ** added to the result array, add the first array's current value.
+            */
             resultArray += [arrayOne[indexOne]]
             indexOne += 1
         }
+        /* In some cases, the first array will be emptied, and the second array
+        ** will have larger values still not added to the merged array. Add them now.
+        */
         if indexTwo < (arrayTwo.count) {
             resultArray += Array(arrayTwo[indexTwo..<arrayTwo.count])
         }
 
-        print("Result size: \(resultArray.count)")
         return resultArray
     }
 
